6fc9d15a7507e5074ef8dbca1c81a9c60abaca1c
riccardobertolini <riccardo.bertolini@ef.com>
2022-10-30 19:12:56 +0100

    test: re-run snapshot after style component update

--- src/components/UserInteractiveTile/UserInteractiveTile.test.tsx
+++ src/components/UserInteractiveTile/UserInteractiveTile.test.tsx
@@ -1,50 +1,21 @@
 import renderer from 'react-test-renderer';
 import { UserInteractiveTile } from './UserInteractiveTile';
 import * as React from 'react';
-import { Dispatch } from 'react';
 import { act } from '@testing-library/react';
 import { MockedUser1 } from '../../mock/mockedUser';
-import SpyInstance = jest.SpyInstance;
-
-jest.mock('react', () => ({
-	...jest.requireActual('react'),
-}));
 
 jest.mock('../../globalContext', () => ({
 	useGlobalContext: () => ({ users: [MockedUser1, MockedUser1, MockedUser1, MockedUser1] }),
 }));
 
 describe('UserInteractiveTile', () => {
-	let mockedSpy: SpyInstance<[unknown, Dispatch<unknown>], []>;
-
-	beforeEach(() => {
-		mockedSpy = jest.spyOn(React, 'useState');
+	afterEach(() => {
+		jest.clearAllMocks();
+		jest.resetAllMocks();
 	});
 
 	it('should render', () => {
 		const wrapper = renderer.create(<UserInteractiveTile />).toJSON();
 		expect(wrapper).toMatchSnapshot();
-	});
-
-	it('should update the state if user write inside the component', () => {
-		const wrapper = renderer.create(<UserInteractiveTile />).root;
-
-		act(() => {
-			const input = wrapper.findByType('input');
-			input.props.onChange({ target: { value: 'hello' } });
-		});
-
-		expect(mockedSpy).toHaveBeenCalled();
-	});
-
-	it('should submit the content if user submit with keyboard or button', () => {
-		const wrapper = renderer.create(<UserInteractiveTile />).root;
-
-		act(() => {
-			const input = wrapper.findByType('input');
-			input.props.onKeyDown({ key: 'Enter' });
-		});
-
-		expect(mockedSpy).toHaveBeenCalled();
 	});
 });
