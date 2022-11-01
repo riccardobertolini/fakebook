d57b09acc0da4f971f7ed7dfa85a4cd56ceb706d
Riccardo Bertolini <info@riccardobertolini.net>
2022-11-01 22:27:04 +0100

    feat: adding vercel tooling

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { inject } from '@vercel/analytics';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+inject();
 root.render(
 	<React.StrictMode>
 		<BrowserRouter>
